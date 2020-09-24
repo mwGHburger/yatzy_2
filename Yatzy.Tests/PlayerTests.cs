baa7e45898644e79039a7d62865808faed7289f7
Michael Wang <michael.wang@myob.com>
2020-09-24 18:48:58 +1000

    added tests for Score Class

---
+++ Yatzy.Tests/PlayerTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace Yatzy.Tests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void ShouldRollDice()
+        {
+            // var player = new Player();
+
+            // We need to check if roll() is called in the Dice class.
+        }
+    }
+}
--- Yatzy/Program.cs
+++ Yatzy/Program.cs
@@ -6,7 +6,10 @@ namespace Yatzy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var score = new Score();
+            var arr = new int[5] {1,1,2,2,2};
+            System.Console.WriteLine(Array.FindAll(arr, x => x == arr[3]).Length);
+
         }
     }
 }
